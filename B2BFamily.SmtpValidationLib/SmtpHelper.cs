b04b7583c667eb92d563671b7f23d05bc25acd55
nkazakov <kazakov.nickolay@gmail.com>
2016-03-20 11:53:43 +0300

    repository init

---
+++ B2BFamily.SmtpValidationLib/SmtpHelper.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net;
+using System.Text;
+
+using B2BFamily.SmtpValidation.Lib.SmtpConnector;
+
+namespace B2BFamily.SmtpValidation.Lib {
+    public class SmtpHelper {
+        public static bool ValidateCredentials(string login, string password, string server, int port, bool enableSsl) {
+            SmtpConnectorBase connector;
+            if (enableSsl) {
+                connector = new SmtpConnectorWithSsl(server, port);
+            } else {
+                connector = new SmtpConnectorWithoutSsl(server, port);
+            }
+
+            if (!connector.CheckResponse(220)) {
+                return false;
+            }
+
+            connector.SendData($"HELO {Dns.GetHostName()}{SmtpConnectorBase.EOF}");
+            if (!connector.CheckResponse(250)) {
+                return false;
+            }
+
+            connector.SendData($"AUTH LOGIN{SmtpConnectorBase.EOF}");
+            if (!connector.CheckResponse(334)) {
+                return false;
+            }
+
+            connector.SendData(Convert.ToBase64String(Encoding.UTF8.GetBytes($"{login}")) + SmtpConnectorBase.EOF);
+            if (!connector.CheckResponse(334)) {
+                return false;
+            }
+
+            connector.SendData(Convert.ToBase64String(Encoding.UTF8.GetBytes($"{password}")) + SmtpConnectorBase.EOF);
+            if (!connector.CheckResponse(235)) {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
